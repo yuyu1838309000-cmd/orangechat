5b6769aecac5d1b5a5305fce41e0c3d88a3b0910
re-ovo <1609403959@qq.com>
2026-02-01 15:51:44 +0800

    chore(build): 更新版本号至1.9.0-beta.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 135
-        versionName = "1.8.2"
+        versionName = "1.9.0-beta.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
